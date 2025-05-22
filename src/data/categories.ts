597f9aefa86f2dd2ad7631b0ee01848148b03424
SantiagoPQ2 <santiago@ibarazi.com.ar>
2025-05-22 12:20:40 -0300

    Add files via upload

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,9 +1,8 @@
 const categories = [
-  { id: 'electronics', name: 'Electronics' },
-  { id: 'clothing', name: 'Clothing' },
-  { id: 'home', name: 'Home & Kitchen' },
-  { id: 'books', name: 'Books' },
-  { id: 'sports', name: 'Sports & Outdoors' },
+  { id: 'hamburguesas', name: 'Hamburguesas' },
+  { id: 'salchichas', name: 'Salchichas' },
+  { id: 'fiambres', name: 'Fiambres' },
+  { id: 'fideos', name: 'Fideos' },
 ];
 
 export default categories;
